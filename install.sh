a615e6cb998bd9c7608efa504725b99c1d94c2d2
niklas gadeberg <spiler2512@gmail.com>
2025-10-21 14:19:19 +0200

    Update install script to use secure download options for my-getjson and my-getinfo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # download my-getjson and my-getinfo scripts from GitHub
-curl -o /usr/bin/my-getjson https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getjson
-curl -o /usr/bin/my-getinfo https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getinfo
+curl -ofsSL /usr/bin/my-getjson https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getjson
+curl -ofsSL /usr/bin/my-getinfo https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getinfo
 chmod +x /usr/bin/my-getjson
 chmod +x /usr/bin/my-getinfo
 
