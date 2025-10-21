3a20b51b524eaf34fce9ecc0edc6698d8628ebe7
niklas gadeberg <spiler2512@gmail.com>
2025-10-21 14:51:22 +0200

    Enhance install script with download progress messages for my-getjson and my-getinfo

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 # download my-getjson and my-getinfo scripts from GitHub
-curl -fsSL https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getjson -o /usr/bin/my-getjson
-curl -fsSL https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getinfo -o /usr/bin/my-getinfo
+
+echo "Downloading my-getjson script..."
+curl -# -fsSL https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getjson -o /usr/bin/my-getjson
+
+echo "Downloading my-getinfo script..." 
+curl -# -fsSL https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getinfo -o /usr/bin/my-getinfo
+
+echo "Setting execute permissions..."
 chmod +x /usr/bin/my-getjson
 chmod +x /usr/bin/my-getinfo
 
