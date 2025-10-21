476da41517d194eb39c3ceb510c37d227d868217
niklas gadeberg <spiler2512@gmail.com>
2025-10-21 14:53:12 +0200

    Remove progress indicator from curl commands in install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 # download my-getjson and my-getinfo scripts from GitHub
 
 echo "Downloading my-getjson script..."
-curl -# -fsSL https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getjson -o /usr/bin/my-getjson
+curl -fsSL https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getjson -o /usr/bin/my-getjson
 
 echo "Downloading my-getinfo script..." 
-curl -# -fsSL https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getinfo -o /usr/bin/my-getinfo
+curl -fsSL https://raw.githubusercontent.com/agent2512/my-rpi-scripts/main/my-getinfo -o /usr/bin/my-getinfo
 
 echo "Setting execute permissions..."
 chmod +x /usr/bin/my-getjson
